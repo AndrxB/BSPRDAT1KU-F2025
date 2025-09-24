ec670a488b8b1fe6c5aebda65a0d8c6ed6913437
Lukas <lukasvranic@gmail.com>
2025-09-24 12:55:29 +0200

    more changes

--- alias.sh
+++
@@ -1,3 +0,0 @@
-alias fslex='dotnet fsLexer/FsLexYacc.11.3.0/build/fslex/net6.0/fslex.dll'
-alias fsyacc='dotnet fsLexer/FsLexYacc.11.3.0/build/fsyacc/net6.0/fsyacc.dll '
-alias fsi='dotnet fsi -r fsLexer/FsLexYacc.11.3.0/build/fsyacc/net6.0/FsLexYacc.Runtime.dll assignment03/Three5/Absyn.fs assignment03/Three5/ExprPar.fs assignment03/Three5/ExprLex.fs assignment03/Three5/Parse.fs'
