3d776a984d49917823cf0e73bcd9063203824b93
Andreas Løvsgren Nielsen <anln@itu.dk>
2025-09-26 12:53:26 +0200

    added runnable commands in Fun

---
+++ assignment05/Fun/aliases.ps1
@@ -0,0 +1,23 @@
+# aliases.ps1 - F# development aliases
+
+function fslex {
+    dotnet ..\..\fsLexer\FsLexYacc.11.3.0\build\fslex\net6.0\fslex.dll @args
+}
+
+function fsyacc {
+    dotnet ..\..\fsLexer\FsLexYacc.11.3.0\build\fsyacc\net6.0\fsyacc.dll @args
+}
+
+function fsi {
+    dotnet fsi -r ..\..\fsLexer/FsLexYacc.11.3.0/build/fsyacc/net6.0/FsLexYacc.Runtime.dll Util.fs Absyn.fs FunPar.fs FunLex.fs Parse.fs @args
+}
+
+function fsiforreal {
+    dotnet fsi -r ..\../fsLexer/FsLexYacc.11.3.0/build/fsyacc/net6.0/FsLexYacc.Runtime.dll Util.fs Absyn.fs FunPar.fs FunLex.fs Parse.fs Fun.fs ParseAndRun.fs @args
+}
+
+function fsihigher {
+    dotnet fsi Absyn.fs HigherFun.fs @args
+}
+
+Write-Host "F# aliases loaded: fslex, fsyacc, fsi, fsiforreal, fsihigher" -ForegroundColor Green
