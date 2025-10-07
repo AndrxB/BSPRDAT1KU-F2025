fe093255bfdd0e88fe10d200531921e0b212893f
Andreas Løvsgren Nielsen <anln@itu.dk>
2025-10-07 13:55:26 +0200

    Everything except 7.2.c

---
+++ assignment06/MicroC/Seven2a.c
@@ -0,0 +1,27 @@
+﻿
+void main(int m) {
+    int i; 
+    
+    int arr[4];
+    arr[0] = 7;
+    arr[1] = 13;
+    arr[2] = 9;
+    arr[3] = 8;
+    
+    int *sum;
+    *sum = 0;
+    arrsum(4, arr, sum);
+    print *sum;
+    
+}
+
+void arrsum(int n,int arr[], int *sump){
+
+    int count;
+    count = 0;
+    while (count < n) {
+        *sump = *sump + arr[count];
+        count = count + 1;
+    }
+    
+}
---
+++ assignment06/MicroC/Seven2b.c
@@ -0,0 +1,38 @@
+﻿
+void main(int m) {
+    int i; 
+    
+    int arr[20];
+
+    int *sum;
+    *sum = 0;
+
+    
+    squares(20, arr);
+    
+    arrsum(20, arr, sum);
+    print *sum;
+    
+}
+
+void squares(int n,int arr[]){
+
+    int count;
+    count = 0;
+    while (count < n) {
+        *(arr + count) = count * count;
+        count = count + 1;
+    }
+    
+}
+
+void arrsum(int n,int arr[], int *sump){
+
+    int count;
+    count = 0;
+    while (count < n) {
+        *sump = *sump + arr[count];
+        count = count + 1;
+    }
+    
+}
